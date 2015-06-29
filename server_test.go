c57d97c658f7b440c139e19717c529f0d0df79bd
Geoff Clitheroe <g.clitheroe@gns.cri.nz>
2015-06-30 06:09:41 +1200

    Update test data.

--- server_test.go
+++ server_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"github.com/GeoNet/msg"
 	_ "github.com/lib/pq"
 	"log"
 	"net/http"
@@ -12,7 +13,36 @@ var ts *httptest.Server
 
 // setup starts a db connection and test server then inits an http client.
 func setup() {
+	// load some test data.  Needs a write user.
 	var err error
+	config.DataBase.User = "hazard_w"
+	db, err = sql.Open("postgres", config.DataBase.Postgres())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = db.Ping()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// TODO add some more data and check the size of some region queries?
+	q := msg.ReadSC3ML07("etc/test/files/2013p407387.xml")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = saveQuake(q)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	db.Close()
+
+	// switch back to the correct user for the tests.
+	// hazard_r can read haz and impact.
+	config.DataBase.User = "hazard_r"
 	db, err = sql.Open("postgres", config.DataBase.Postgres())
 	if err != nil {
 		log.Fatal(err)
